c5fc25643e5f7e9d70868668fde413a66f10ea55
Sudo Space <abolfazleffati1381@gmail.com>
2023-07-07 17:43:32 +0330

    Fix can't connect after change transmission type in inbound

--- web/assets/js/model/xray.js
+++ web/assets/js/model/xray.js
@@ -1735,7 +1735,7 @@ Inbound.VLESSSettings.VLESS = class extends XrayCommonClass {
     constructor(id=RandomUtil.randomUUID(), flow='', email=RandomUtil.randomText(), totalGB=0, expiryTime=0, enable=true, tgId='', subId='') {
         super();
         this.id = id;
-        this.flow = flow;
+        this.flow = this.isTcp ? flow : '';
         this.email = email;
         this.totalGB = totalGB;
         this.expiryTime = expiryTime;
